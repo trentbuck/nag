0a53e2d5858deab09863958d009497b95ef97934
Trent W. Buck <trentbuck@gmail.com>
2010-07-13 19:19:04 +1000

    new timesheet nagger & export scripts.

---
+++ nag.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -ueE
+## Problem: I forget to file timesheets, and I'm easily distracted
+## such that I forget WHEN I changed from working on ticket N to
+## working on ticket M.
+##
+## Therefore, run this from a crontab every fifteen minutes.  It will
+## pop up an editor in my screen session, prompting me to write down
+## what I'm doing RIGHT NOW.
+##
+## Do nothing if the previous popup is still open, so that after being
+## AFK I don't return to a session with hundreds of popups.
+##
+## FIXME: use sqlite or tokyocabinet so backdated timestamps will be
+## sorted correctly.
+
+while getopts "hpd:" opt
+do  case "$opt" in
+        d)  date=$OPTARG;;
+        p)                      # cron is trying to popup a window.
+            test -n "$(find /dev/tty?* /dev/pts -user "$EUID" -mmin -14 -print -quit)"
+            getent hosts flora.cyber.com.au >/dev/null
+            exec screen -X screen -t nag ssh flora.cyber.com.au -t ~/.bin/nag
+            ;;
+        h|'?')
+            echo "Usage: nag [ -p | [-dDATE] [MESSAGE] ]"
+            echo "In your crontab, try:"
+            echo "    PATH=/home/twb/.bin:/usr/local/bin:/usr/bin:/bin"
+            echo "    */15 *  * * *  nag -p"
+            exit 1;;
+    esac
+done
+shift $((${OPTIND:-1}-1))
+
+now="$(date -d"${date-now}" --rfc-3339 seconds)"
+
+if test 0 -eq $#
+then
+    toilet -tffuture "What are you doing?!" || test 127 = $?
+    if test -f ~/.timelog
+    then
+        tail ~/.timelog
+        old=$(sed -rn '$ s/^.{26}//p' ~/.timelog)
+    fi
+    read -ep "$now " -i "${old-}" response
+    set -- "$response"
+fi
+
+echo "$now $*" >>~/.timelog
---
+++ nag2al.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eEu
+PATH=$PATH:/usr/src/alloc/bin   # YUK.
+trap 'echo >&2 "$0: unknown error"' ERR
+
+day1= time1= task1= comment1=
+while read day time task comment
+do
+    [[ $task =~ ^[[:digit:]]+$ ]] ||
+    comment="$task${comment:+ $comment}" task=?????
+
+    # Truncate to the zeroth second.  This prevents the period
+    # 10:00:00 to 10:15:01 being rounded up to a half-hour.
+    time="${time%:??[+-]??:??}:00${time#??:??:??}"
+
+    # Unless the task/comment has changed, this isn't a "real" edge,
+    # so proceed on to the next one (without setting the foo1 old
+    # flavours).
+    test "$task1$comment1" != "$task$comment" || continue
+
+    if test -n "$day1"         # we've seen at least one entry before.
+    then
+        dur=$((($(date -d"$day $time" +%s) - $(date -d"$day1 $time1" +%s))))
+        dur=$(((dur/900) + !!(dur%900)))                  # round to nearest 15min
+        [[ $comment1 =~ ^(email|food|home|fnord.*)$ ]] || # skip boring entries
+        {
+            echo alloc work -qt"$task1" -d"$day1" -h"$((dur*15))m" -c"${comment1:-NO COMMENT} [${time1%?????????}]"
+            alloc work -qt"$task1" -d"$day1" -h"$((dur*15))m" -c"${comment1:-NO COMMENT} [${time1%?????????}]" || echo FAILED
+        }
+    fi
+    day1=$day time1=$time task1=$task comment1=$comment
+done < <(cat "$@" | sort)
