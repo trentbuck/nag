084797fd4f68db7d906eea55d48a085532797109
Trent W. Buck <twb@cyber.com.au>
2014-02-15 19:05:40 +1100

    rewrite to popup on laptop, but write to flora.

--- nag.sh
+++ nag.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ueE
+#!/bin/bash
 ## Problem: I forget to file timesheets, and I'm easily distracted
 ## such that I forget WHEN I changed from working on ticket N to
 ## working on ticket M.
@@ -12,21 +12,37 @@
 ##
 ## FIXME: use sqlite or tokyocabinet so backdated timestamps will be
 ## sorted correctly.
+##
+## In your crontab, try:
+##     PATH=/home/twb/.bin:/usr/local/bin:/usr/bin:/bin
+##     */15 *  * * *  INSIDE_CRON=true nag
 
-while getopts "hpd:" opt
+set -eEu
+set -o pipefail
+trap 'echo >&2 "${0##*/}:${LINENO}: unknown error"' ERR
+
+# Cron is trying to popup a window.
+if test -n "${INSIDE_CRON-}"
+then
+    test -n "$(find /dev/tty?* /dev/pts -user "$EUID" -mmin -14 -print -quit)" || exit 0
+    getent hosts flora.cyber.com.au >/dev/null || exit 0
+    exec screen -X screen -t nag ssh flora.cyber.com.au -t ~/.bin/nag -w
+fi
+
+
+# This is the laptop where I type.
+[[ $HOSTNAME == flora ]] ||
+exec ssh flora.cyber.com.au -t ~/.bin/nag "$@"
+
+
+# This is flora, where the file is read/written.
+while getopts wd: opt
 do  case "$opt" in
-        d)  date=$OPTARG;;
-        p)                      # cron is trying to popup a window.
-            test -n "$(find /dev/tty?* /dev/pts -user "$EUID" -mmin -14 -print -quit)"
-            getent hosts flora.cyber.com.au >/dev/null
-            exec screen -X screen -t nag ssh flora.cyber.com.au -t ~/.bin/nag
-            ;;
-        h|'?')
-            echo "Usage: nag [ -p | [-dDATE] [MESSAGE] ]"
-            echo "In your crontab, try:"
-            echo "    PATH=/home/twb/.bin:/usr/local/bin:/usr/bin:/bin"
-            echo "    */15 *  * * *  nag -p"
-            exit 1;;
+        # Force me to stop and think.
+        # FIXME: doesn't discard stuff I type while doing this.
+        w) wait=true;;
+        # Let's me backdate entries easily.
+        d) date=$OPTARG;;
     esac
 done
 shift $((${OPTIND:-1}-1))
@@ -38,20 +54,30 @@ then
     toilet -tffuture "What are you doing?!" || test 127 = $?
     if test -f ~/.timelog
     then
-        tail ~/.timelog
-        old=$(sed -rn '$ s/^.{26}//p' ~/.timelog)
-
-        # Allocate new history file and delete it on exit.
-        HISTFILE=$(mktemp -t nag.XXXXXX)
-        trap "rm -f \"$HISTFILE\"" 0 TERM ERR QUIT
+        tail ~/.timelog     # print the last few entries.
 
         # Store a single copy of all unique entries in history file.
-        sed -rn 's/^.{26}//p' ~/.timelog | uniq > $HISTFILE
+        HISTFILE=$(mktemp -t nag.XXXXXX)
+        cut -c27- ~/.timelog | uniq | tail -128 >"$HISTFILE"
         history -c                      # Clear all current history.
         history -n                      # Read everything from history file.
+        old=$(tail -1 "$HISTFILE")
+        rm -f "$HISTFILE"
     fi
-    read -ep "$now " -i "${old-}" response
+    test -z "${wait-}" || sleep 5
+    read -ep $'\1'"$(tput setaf 2||:)"$'\2'"$now"$'\1'"$(tput sgr0||:)"$'\2'" " -i "${old-}" response
     set -- "$response"
 fi
 
 echo "$now $*" >>~/.timelog
+
+# 18:28 <twb> The problem I have is that (I think) read is assuming all the prompt characters are printable, so if I press <up> and <down>, the completion is too far to the left and the line gets scrambled.
+# 18:29 <twb> I tried adding \[ and \], like one does for PS1, but they just got printed.
+# 18:33 <pgas> twb: use $'\1' $'\2' instead of \[ \]
+# 18:33 <twb> Where is that documented?
+# 18:34 <pgas> it's not
+# 18:34 <twb> Not even in rluserman or something? :-)
+# 18:34 <pgas> ah well, maybe I don't know
+# 18:35 <pgas> Chet has gave this answer to the bug-bash list, so you can rely on it ;)
+# 18:35 <pgas> has given
+# 18:53 <Riviera> twb: RL_PROMPT_START_IGNORE and RL_PROMPT_END_IGNORE in readline.h
