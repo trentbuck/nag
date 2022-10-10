7205eb515c26db6e726a60341f9ab05ac76f1d87
Trent W. Buck <trentbuck@gmail.com>
2022-10-11 07:20:57 +1100

    be a bit clearer about the fake timestamp event's purpose
    07:17 <twb> Classy.  This output still has exit(0) so chronic was suppressing it:
    07:17 <twb>     alloc work -qt23321 -d2022-10-10 -h480m -cbrain fried [10:25]
    07:17 <twb>     alloc work -qt????? -d2022-10-10 -h15m -cfnord [18:20]
    07:17 <twb>     FAILED
    07:19 <twb> Ah and that's because 1) my old nag used to automatically skip time events that were exactly "fnord"; and 2) "alloc submit" is broken and always returns success

--- nag2al-wrapper.sh
+++ nag2al-wrapper.sh
@@ -11,7 +11,7 @@ grep "^$(date -I -dyesterday)" ~/.timelog |
 # Add a fake timestamp to the bottom.
 # This might make nag2al create a timestamp entry if I forget to clock out?
 # I do not remember exactly why this is here, and the old cron job didn't say.
-awk '{print}END{print $1, $2, "fnord"}' |
+awk '{print}END{print $1, $2, "23321 dummy time event to hopefully cause the previous real event to actually get logged into alloc"}' |
 
 # Do the nag2al stuff.
 nag2al |
