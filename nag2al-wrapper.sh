e440a93e3d9d3c4d20ab78844ed4564fc4353c73
Trent W. Buck <trentbuck@gmail.com>
2022-10-18 15:52:57 +1100

    avoid noise in weekend logcheck emails (to sysadmin@)
    I saw this:
    
        Oct 17 05:03:33 heavy nag2al-wrapper[3833466]: Null message body; hope that's ok
    
    Also capture error output to the email, rather than the journal.
    That is kind of shitty and I am ambivalent about it.

--- nag2al-wrapper.sh
+++ nag2al-wrapper.sh
@@ -5,16 +5,22 @@
 
 PATH="$PATH:$HOME/.local/bin"   # YUK.
 
-# Get all events with yesterday's timestamp.
-grep "^$(date -I -dyesterday)" ~/.timelog |
+{
 
-# Add a fake timestamp to the bottom.
-# This might make nag2al create a timestamp entry if I forget to clock out?
-# I do not remember exactly why this is here, and the old cron job didn't say.
-awk '{print}END{print $1, $2, "23321 dummy time event to hopefully cause the previous real event to actually get logged into alloc"}' |
+    # Get all events with yesterday's timestamp.
+    if ! grep "^$(date -I -dyesterday)" ~/.timelog
+    then echo "There were no nag logs yesterday" >&2
+    fi |
 
-# Do the nag2al stuff.
-nag2al |
+    # Add a fake timestamp to the bottom.
+    # This might make nag2al create a timestamp entry if I forget to clock out?
+    # I do not remember exactly why this is here, and the old cron job didn't say.
+    awk '{print}END{print $1, $2, "23321 dummy time event to hopefully cause the previous real event to actually get logged into alloc"}' |
+
+    # Do the nag2al stuff.
+    nag2al
+
+} |&
 
 # Instead of putting stuff into "journalctl --user",
 # which ends up in logcheck@ emails,
